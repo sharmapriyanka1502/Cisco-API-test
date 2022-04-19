f0962a07d1c58c50f16db7250bcf320fd2764874
sharmapriyanka1502 <62370963+sharmapriyanka1502@users.noreply.github.com>
2022-04-19 13:49:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-    return "sharmapriyanka2585@gmail.com"
+def index():
+    return "index-test"
     
 @app.route("/tac_status")
 def tac_status():
